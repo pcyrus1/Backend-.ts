11201c108f7c5637ff7ecb8a3ecc47a4e3fd307d
Prince <princedonkor101@gmail.com>
2024-02-16 11:52:04 +0000

    change

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("DB", "USERNAME", "PASSWORD", {
+const sequelize = new Sequelize("Db", "USERNAME", "PASSWORD", {
   host: "localhost",
   dialect: "mysql",
 });
